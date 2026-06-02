2e51d2acda77d8c452ffdc2cf1bc824b21dcd983
Guzman <gabe.guzman@tylertech.com>
2026-06-02 14:39:46 -0400

    Fix General tab: add Map Updates, correct Login Type options, radio buttons for toggles, no Cancel; fix tab names

--- src/MTWeb/Models/TylerDrive/Setup/GeneralSetupModel.cs
+++ src/MTWeb/Models/TylerDrive/Setup/GeneralSetupModel.cs
@@ -2,7 +2,8 @@ namespace MTWeb.Models.TylerDrive.Setup;
 
 public class GeneralSetupModel
 {
-    public bool EnableAutoLogout { get; set; }
+    public bool MapUpdatesEnabled { get; set; }
+    public string LoginType { get; set; } = "PinOnly";
+    public bool AutoLogoutEnabled { get; set; }
     public int AutoLogoutMinutes { get; set; } = 15;
-    public string LoginType { get; set; } = "EmployeePin";
 }
